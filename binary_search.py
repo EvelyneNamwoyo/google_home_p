06276fcd2eafd00b6bbb77ffa819b2e7794e9a7c
Evelyne <namwoyoeve@gmail.com>
2017-04-20 21:52:13 +0300

    added the binary_search function with its correspoding test file

--- binary_search.py
+++ binary_search.py
@@ -1,27 +1,42 @@
+
 class BinarySearch(list):
     def __init__(self,a,b):
         self.a=a
         self.b=b
-        arr= [x*2 for x in range(1,b+1)]
-        lenght=len(arr)
+        self.arr=range(b,a*b+1,b)
+        list.__init__(self,self.arr)
+        self.length=len(self.arr)
 
 
     def search(self,val):
         count=0
-        low=0
-        high=len(arr)-1
-        while(low<=high):
-            mid=(low+high)/2
-            if li[mid]==val:
-                return mid
-            elif li[mid]<val:
-                low=mid+1
-            elif li[mid]>val:
-                high=mid+1
-            count+=1
-        return {count:li[mid]}
+        lobound=0
+        hibound=len(self.arr)-1
 
-a=BinarySearch(6,2)
-print(a.search(7))
-# x = [x*2 for x in range(1,6+1)] 
-# print(x)
+        if val not in self.arr:
+            return {'count':count, 'index':-1}
+        else:
+
+            while(lobound<=hibound):
+                mid=(lobound+hibound)//2
+                if self.arr[lobound]==val:
+                    return {'count':count, 'index':lobound}
+
+                elif self.arr[hibound]==val:
+                    return {'count':count, 'index':hibound}
+
+                elif self.arr[mid]==val:
+                    return {'count':count, 'index':mid}
+
+                elif self.arr[mid]<val:
+                    lobound=mid+1
+                    count+=1
+
+                elif self.arr[mid]>val:
+                    count+=1
+                    hibound=mid-1
+
+            return {'count':count,'index':mid}
+
+a=BinarySearch(16,1)
+print(a.search(5))
